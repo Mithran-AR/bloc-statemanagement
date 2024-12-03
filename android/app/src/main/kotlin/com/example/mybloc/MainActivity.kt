9f9e3f107823a3b1c4ba766b696db497e1978c06
Mithran-AR <mithranar000@gmail.com>
2024-12-03 17:27:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/mybloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mybloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/counter/counter_event.dart
@@ -0,0 +1,6 @@
+part of 'counter_bloc.dart';
+
+ class CounterEvent {}
+class Increment extends CounterEvent{}
+class Decrement extends CounterEvent{}
+
